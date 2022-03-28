fa70d3cbe5e72300244fa1d443fb2b2f221997b3
Konstantin Beloborodov <kosta.white@gmail.com>
2022-03-28 22:08:33 +0300

    rename var names

--- datacenter/passcard_info_view.py
+++ datacenter/passcard_info_view.py
@@ -5,16 +5,16 @@ from django.shortcuts import render
 
 def passcard_info_view(request, passcode):
     passcard = Passcard.objects.get(passcode=passcode)
-    serialized_visits = Visit.objects.filter(passcard=passcard)
-    this_passcard_visits = []
-    for visit in serialized_visits:
-        this_passcard_visits.append({
+    visits = Visit.objects.filter(passcard=passcard)
+    serialized_this_passcard_visits = []
+    for visit in visits:
+        serialized_this_passcard_visits.append({
             'entered_at': visit.entered_at,
             'duration': visit.get_duration(),
             'is_strange': visit.is_visit_long()
         })
     context = {
         'passcard': passcard,
-        'this_passcard_visits': this_passcard_visits
+        'this_passcard_visits': serialized_this_passcard_visits
     }
     return render(request, 'passcard_info.html', context)
--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -4,17 +4,17 @@ from django.shortcuts import render
 
 
 def storage_information_view(request):
-    serialized_active_visits = Visit.objects.filter(leaved_at__isnull=True)
-    non_closed_visits = []
-    for visit in serialized_active_visits:
-        non_closed_visits.append({
+    active_visits = Visit.objects.filter(leaved_at__isnull=True)
+    serialized_non_closed_visits = []
+    for visit in active_visits:
+        serialized_non_closed_visits.append({
                     'who_entered': visit.passcard.owner_name,
                     'entered_at': visit.entered_at,
                     'duration': format_duration(visit.get_duration()),
                     'is_strange': visit.is_visit_long()
         })
     context = {
-        'non_closed_visits': non_closed_visits,
+        'non_closed_visits': serialized_non_closed_visits,
     }
     return render(request, 'storage_information.html', context)
 
