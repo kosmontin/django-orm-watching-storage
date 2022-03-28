c955a3acf76f04bfd7125a458a7e8c3f1bb2390e
Konstantin Beloborodov <kosta.white@gmail.com>
2022-03-28 07:26:40 +0300

    remove all unnecessary comments

--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -1,5 +1,3 @@
-import datetime
-
 from datacenter.models import Passcard
 from datacenter.models import Visit
 from django.shortcuts import render
@@ -16,7 +14,7 @@ def storage_information_view(request):
                     'is_strange': visit.is_visit_long()
         })
     context = {
-        'non_closed_visits': non_closed_visits,  # не закрытые посещения
+        'non_closed_visits': non_closed_visits,
     }
     return render(request, 'storage_information.html', context)
 
